f07216413c0ba2bfc5d5b235c3c7b9c4d4d0dade
raffibromabar-web <raffibromabar@gmail.com>
2026-04-03 15:02:22 +0700

    membuat sistem pengecekan batas maksimal input

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -4,3 +4,21 @@ using namespace std;
 int arr[20], B[20];
 
 int n;
+
+void input()
+{
+    while (true)
+    {
+        cout << "Masukkan Panjang element array : ";
+        cin >> n;
+
+        if (n <= 20)
+        {
+            break;
+        }
+        else
+        {
+            cout << "\nMaksimal panjang array adalah 20";
+        }
+    }
+}
