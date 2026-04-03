9ae1ab968c33fd5f56cbbcaf7efad791e8516706
raffibromabar-web <raffibromabar@gmail.com>
2026-04-03 15:30:42 +0700

    menyalin sisa angka dari kelompok kanan ke dalam kotak sementara

--- mergeSort.cpp
+++ mergeSort.cpp
@@ -62,4 +62,11 @@ void mergeSort(int low, int high)
         }
         k++;
     }
+
+    while (j <= high) // step 4.e
+    {
+        B[k] = arr[j]; // step 4.e.i
+        j++;           // step 4.e.ii
+        k = k + 1;     // step 4.e.iii
+    }
 }
